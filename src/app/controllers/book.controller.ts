e8c049cdd69af45706d083fcf9e00b159e3ef4b3
rjazmirkhan <bangladeshfreelancerazmir@gmail.com>
2025-06-21 22:53:03 +0600

    GET /api/books and  /api/books/id Complete

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -3,7 +3,7 @@ import express from"express"
 import { schemaBook } from "../models/book.model";
  export const bookRouters = express.Router()
 
-bookRouters.post('/books', async (req, res) => {
+bookRouters.post ('/books', async (req, res) => {
     const bookBody = req.body
     const book = await schemaBook.create(bookBody)
     res.status(201).json({
@@ -13,6 +13,26 @@ bookRouters.post('/books', async (req, res) => {
     })
 })
 
+bookRouters.get ('/books/:id', async (req, res) => {
+    const id = req.params.id
+    const book = await schemaBook.findById(id)
+    res.status(201).json({
+        succese: true,
+        message: "Book er one Data Find kora oice",
+        book: book
+    })
+})
+
+
+bookRouters.get ('/books', async (req, res) => {
+    // const bookBody = req.body
+    const book = await schemaBook.find()
+    res.status(201).json({
+        succese: true,
+        message: "Book er All Data Find kora oice",
+        book: book
+    })
+})
 
 
 
